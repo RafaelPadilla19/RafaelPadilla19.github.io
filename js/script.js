d978e249abf5daba6bd402cfdcbfe7011220f571
Rafael Padilla <carlos09.cp65@gmail.com>
2021-07-04 22:14:25 -0600

    Agregar funcionalidad del model content, con un video de youtube incrustado y que se adapte a movil

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const _portfolio= document.querySelector('.sec-portfolio-js');
+// Para mostrar la misma imagen del la seccion portafolio en el modal
+
+/* const _portfolio= document.querySelector('.sec-portfolio-js');
 const _modalImgPortfolio= document.querySelector('.img-modal-js');
 
 _portfolio.addEventListener('click',(e)=>{
@@ -8,6 +10,28 @@ _portfolio.addEventListener('click',(e)=>{
         _modalImgPortfolio.src=urlImg;
 
     }
+}); */
+
+// Cerrar un video al cerrar un modal.
+
+
+let btnCerrar = document.getElementById('btn-close');
+btnCerrar.addEventListener('click',()=>{
+
+    let video= document.getElementById('video_youtube');
+    let urlVideo= video.src;
+
+    video.src='';
+    video.src=urlVideo;
 });
 
+let modal= document.getElementById('modal-portfolio');
+modal.addEventListener('click',()=>{
 
+    let video= document.getElementById('video_youtube');
+    let urlVideo= video.src;
+
+    video.src='';
+    video.src=urlVideo;
+    
+});
